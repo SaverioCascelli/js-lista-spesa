dea98908f7e5eda5009159b8c4345613bd446395
SaverioCascelli <saverio.cascelli@gmail.com>
2022-10-18 15:02:03 +0200

    for & while circle

--- js/script.js
+++ js/script.js
@@ -7,7 +7,23 @@
 // - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all’interno ciclo while
 
 
-const shopList = ["mele", "pollo", "banane", "carote", "spinaci"];
+const shopList = ["mele", "pollo", "banane", "carote", "spinaci" , "percoche", "rape", ];
 const scFor = document.querySelector(".sc-for");
 const scWhile = document.querySelector(".sc-while");
 
+for(let i = 0; i < shopList.length; i++){
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.innerHTML = shopList[i];
+    scFor.append(li);
+}
+
+let j = 0;
+
+while( j < shopList.length){
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.innerHTML = shopList[j];
+    scWhile.append(li);
+    j++
+}
